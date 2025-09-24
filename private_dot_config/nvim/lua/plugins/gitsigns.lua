b0a15cd3cfd1bbd6b79f4b7e12c4acf66caa1bd5
OnyxPerseus <nguyenminhquang2406@gmail.com>
2025-09-24 13:31:12 +0700

    change binding key function

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,11 +1,14 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = "VeryLazy",
-  config = function ()
+  config = function()
     require("gitsigns").setup({
-      vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { noremap = true, silent = true }),
-      vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true }),
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true }),
+      on_attach = function()
+        vim.keymap.set("n", "<leader>gs", ":Gitsigns stage_hunk<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { noremap = true, silent = true })
+      end
     })
   end
 }
